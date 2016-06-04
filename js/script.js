35b1e4846b3833e15a3258ff3a58ed1b6cab1d19
Vladimir Tyalo <bobtial@inbox.ru>
2016-06-04 16:03:55 +0400

    add new file for user-block amend html scss js adjusting to new html structure

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@
 
 
   var burgerButton = document.querySelector(".top-menu__burger");
-  var modalMenu = document.querySelector(".modal-menu");
+  var modalMenus = document.querySelectorAll(".modal-menu");
 
   burgerButton.addEventListener("click", function (ev) {
     ev.preventDefault();
 
     burgerButton.classList.toggle("top-menu__burger--close");
-    modalMenu.classList.toggle("modal-menu--hidden");
+    console.log(modalMenus.length);
+    for(var i = 0; i < modalMenus.length; i++) {
+      modalMenus[i].classList.toggle("modal-menu--hidden");
+    }
+
+
   });
 
 })();
