3662cb04d545a53348717998f5c080a6dfb46403
Vladimir Tyalo <bobtial@inbox.ru>
2016-06-07 14:40:07 +0400

    modal-menu tablet and desctop reset paddings

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@
 
 
   var burgerButton = document.querySelector(".top-menu__burger");
-  var modalMenus = document.querySelectorAll(".modal-menu");
+  var modalMenu = document.querySelector(".modal-menu");
+  var userBlock = document.querySelector(".user-block");
+
+  userBlock.classList.remove("no-js");
+  modalMenu.classList.remove("no-js");
 
   burgerButton.addEventListener("click", function (ev) {
     ev.preventDefault();
 
     burgerButton.classList.toggle("top-menu__burger--close");
-    console.log("burger/close button is clicked");
-    for(var i = 0; i < modalMenus.length; i++) {
-      modalMenus[i].classList.toggle("modal-menu--hidden");
-    }
+
+    modalMenu.classList.toggle("modal-menu--closed");
+    userBlock.classList.toggle("user-block--closed");
 
 
   });
